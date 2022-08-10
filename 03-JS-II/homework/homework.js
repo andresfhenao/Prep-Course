5fb9df75501c1ebf1944942be325985a90818023
andresfhenao <104663028+andresfhenao@users.noreply.github.com>
2022-08-10 15:13:50 -0500

    complete

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -190,27 +190,52 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if(valor == true){
+    return "Soy verdadero";
+  }
+  else{
+    return "Soy falso";
+  }
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+let array = [];
+for(let n = 0; n<11; n++){
+  array.push(6 * n);
+}
+return array;
   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if(numero > 99 && numero <1000){
+    return true;
+  }
+  return false;
   
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
-  //Usar el bucle do ... while.
+  //Usar el bucle do ... while
+
+var numero = numero;
+var i = 0;
+
+do {
+  i = i + 1;
+  numero = numero + 5;
+} while (i < 8);
+
+return numero;
 }
+
 
 
 // No modificar nada debajo de esta línea
